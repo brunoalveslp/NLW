4591423402f8f691334727de786017ca98f39092
Bruno Alves <brunoalveslp200597@gmail.com>
2020-06-04 04:02:52 -0300

    new files and pages

--- js/create-point.js
+++ js/create-point.js
@@ -7,7 +7,7 @@ function populateUfs(){
     .then( res => res.json() )//buscou e transformou em json maneira completa (res) => {return res,jaon}
     .then( states => {
         for(const state of states){
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        ufSelect.innerHTML += `<option value="${state.sigla}">${state.nome}</option>`
         }
     })
 }
@@ -25,9 +25,12 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citysSelect.innerHTML = "<option>Selecione a Cidade</option>"
+    citysSelect.disabled = true
+
     fetch(url).then( res => res.json() ).then(cities => {
         for( const city of cities){
-            citysSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citysSelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citysSelect.disabled = false
@@ -36,3 +39,35 @@ function getCities(event){
 
 document.querySelector("select[name=uf]")
 .addEventListener("change", getCities)
+
+
+// itens de coleta
+
+let itemsToColect = document.querySelectorAll(".items-grid li")
+for(item of itemsToColect){
+    item.addEventListener("click", handleSelectedItems)
+}
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItems(event){
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")//adicionar classes a tag html
+    const itemId = itemLi.dataset.id
+
+    let alreadySelected = selectedItems.findIndex( function(item){
+        const itemFound = item == itemId
+        return itemFound
+    })
+
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item != itemId
+        return itemIsDifferent//se estiver selecionado tira 
+        })
+        selectedItems = filteredItems
+    } else {//senao estiver selecionado adiciona
+        selectedItems.push(itemId)
+    }
+  collectedItems.value = selectedItems
+}
